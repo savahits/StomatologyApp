37a287035a5b58c99650511dc10f0c130280bac4
Savva <savahits@gmail.com>
2026-03-13 16:59:28 +0300

    feat: create Doctor entity

---
+++ src/main/java/ru/shmelev/stomatologyapp/domain/Doctor.java
@@ -0,0 +1,41 @@
+package ru.shmelev.stomatologyapp.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "doctor")
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Фамилия обязательна")
+    @Size(min = 2, max = 100)
+    @Column(nullable = false)
+    private String surname;
+
+    @NotBlank(message = "Имя обязательно")
+    @Size(min = 2, max = 100)
+    @Column(nullable = false)
+    private String name;
+
+    @Size(max = 100)
+    private String patronymic;
+
+    @NotBlank(message = "Телефон обязателен")
+    @Column(nullable = false)
+    private String phone;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "specialization_id", nullable = false)
+    private Specialization specialization;
+}
