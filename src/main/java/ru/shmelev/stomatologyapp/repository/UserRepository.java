c30f2558431ef02a32e693b24b30fa8e67d1577b
Savva <savahits@gmail.com>
2026-03-18 22:36:54 +0300

    feat: add UserRepository entity

---
+++ src/main/java/ru/shmelev/stomatologyapp/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package ru.shmelev.stomatologyapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.shmelev.stomatologyapp.domain.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String name);
+}
