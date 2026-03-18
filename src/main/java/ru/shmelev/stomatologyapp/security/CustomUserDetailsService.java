3c23d04580c56536f149f7e6e3a7af316e0e2634
Savva <savahits@gmail.com>
2026-03-18 22:54:34 +0300

    feat: add CustomUserDetailsService class

---
+++ src/main/java/ru/shmelev/stomatologyapp/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package ru.shmelev.stomatologyapp.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ru.shmelev.stomatologyapp.domain.User;
+import ru.shmelev.stomatologyapp.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername( String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().getName().replace("ROLE_", ""))
+                .build();
+    }
+}
