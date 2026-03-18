863cf8d3214aa392b2400dba4aa53a01f51a6268
Savva <savahits@gmail.com>
2026-03-18 22:36:48 +0300

    feat: add User entity

---
+++ src/main/java/ru/shmelev/stomatologyapp/domain/User.java
@@ -0,0 +1,35 @@
+package ru.shmelev.stomatologyapp.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+}
