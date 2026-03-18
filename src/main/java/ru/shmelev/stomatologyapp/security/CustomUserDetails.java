a30cd00449bfd55de4c8f960ed6cf42dabff78fd
Savva <savahits@gmail.com>
2026-03-19 00:02:19 +0300

    feat: create CustomUserDetailsService

---
+++ src/main/java/ru/shmelev/stomatologyapp/security/CustomUserDetails.java
@@ -0,0 +1,39 @@
+package ru.shmelev.stomatologyapp.security;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import ru.shmelev.stomatologyapp.domain.User;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().getName()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    public String getFullName() {
+        return user.getFullName();
+    }
+
+}
