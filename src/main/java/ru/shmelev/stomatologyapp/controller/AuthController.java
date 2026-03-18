b7855d527275ecf0a58e522fc83244637573b1f7
Savva <savahits@gmail.com>
2026-03-18 23:01:08 +0300

    feat: add AuthController with @GetMapping("/login")

---
+++ src/main/java/ru/shmelev/stomatologyapp/controller/AuthController.java
@@ -0,0 +1,13 @@
+package ru.shmelev.stomatologyapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "auth/login";
+    }
+}
