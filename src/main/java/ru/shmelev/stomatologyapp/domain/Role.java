8ce8db77ba7c37d437f805b3ef0d518e5dc47b7e
Savva <savahits@gmail.com>
2026-03-18 22:36:43 +0300

    feat: add Role entity

---
+++ src/main/java/ru/shmelev/stomatologyapp/domain/Role.java
@@ -0,0 +1,24 @@
+package ru.shmelev.stomatologyapp.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
