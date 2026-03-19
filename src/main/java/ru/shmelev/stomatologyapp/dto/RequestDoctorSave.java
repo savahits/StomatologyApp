360153f008373445427ba0a12608b7fb5bb8aa8b
Savva <savahits@gmail.com>
2026-03-19 15:48:13 +0300

    feat: add validation for RequestDoctorSave

--- src/main/java/ru/shmelev/stomatologyapp/dto/RequestDoctorSave.java
+++ src/main/java/ru/shmelev/stomatologyapp/dto/RequestDoctorSave.java
@@ -1,5 +1,9 @@
 package ru.shmelev.stomatologyapp.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,11 +13,24 @@ import lombok.Setter;
 @Setter
 public class RequestDoctorSave {
 
+    @NotBlank(message = "Логин обязателен")
     private String username;
+
+    @NotBlank(message = "Фамилия обязательна")
     private String surname;
+
+    @NotBlank(message = "Имя обязательно")
     private String name;
+
     private String patronymic;
+
+    @Pattern(regexp = "^$|^\\+?\\d{7,15}$", message = "Введите корректный телефон")
     private String phone;
+
+    @NotNull(message = "Выберите специализацию")
     private Long specializationId;
+
+    @NotBlank(message = "Пароль обязателен")
+    @Size(min = 6, message = "Пароль должен содержать минимум 6 символов")
     private String password;
 }
