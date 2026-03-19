ace418efb351ff7ff3112a7ffdf8d42dda26dd2b
Savva <savahits@gmail.com>
2026-03-19 14:17:14 +0300

    feat: add create method for DoctorService interface

--- src/main/java/ru/shmelev/stomatologyapp/service/DoctorService.java
+++ src/main/java/ru/shmelev/stomatologyapp/service/DoctorService.java
@@ -1,10 +1,13 @@
 package ru.shmelev.stomatologyapp.service;
 
 import ru.shmelev.stomatologyapp.domain.Doctor;
+import ru.shmelev.stomatologyapp.dto.RequestDoctorSave;
+
 import java.util.List;
 
 public interface DoctorService {
 
     List<Doctor> findAllDoctors();
 
+    void create(RequestDoctorSave dto);
 }
