7bd961480969b6d6ecd00da626da96afc34e7acb
Savva <savahits@gmail.com>
2026-03-20 14:47:20 +0300

    feat: create Client entity

---
+++ src/main/java/ru/shmelev/stomatologyapp/domain/Client.java
@@ -0,0 +1,43 @@
+package ru.shmelev.stomatologyapp.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(
+    name = "client",
+    uniqueConstraints = {
+            @UniqueConstraint(name = "uk_client_phone", columnNames = "phone")}
+)
+@Getter
+@Setter
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "surname", nullable = false, length = 100)
+    private String surname;
+
+    @NotBlank
+    @Size(max = 100)
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Size(max = 100)
+    @Column(name = "patronymic", length = 100)
+    private String patronymic;
+
+    @NotBlank
+    @Pattern(regexp = "^\\+[1-9][0-9]{7,14}$")
+    @Column(name = "phone", nullable = false, length = 20, unique = true)
+    private String phone;
+
+}
