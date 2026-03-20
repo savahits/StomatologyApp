e4f35337aa4738a1bbc709fd73c1270e54c2ba28
Savva <savahits@gmail.com>
2026-03-20 15:03:05 +0300

    feat: create RequestAppointmentCreate DTO

---
+++ src/main/java/ru/shmelev/stomatologyapp/dto/RequestAppointmentCreate.java
@@ -0,0 +1,34 @@
+package ru.shmelev.stomatologyapp.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class RequestAppointmentCreate {
+
+    @NotBlank
+    @Size(max = 100)
+    private String surname;
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    @Size(max = 100)
+    private String patronymic;
+
+    @NotBlank
+    private String phone;
+
+    @NotNull
+    private Long doctorId;
+
+    @NotNull
+    private LocalDateTime time;
+}
