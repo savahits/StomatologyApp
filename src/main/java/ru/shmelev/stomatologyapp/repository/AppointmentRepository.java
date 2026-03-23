1fa935d6a09dc4eb15c8997a7f38656992307bde
Savva <savahits@gmail.com>
2026-03-24 00:27:58 +0300

    feat: add findAllWithClientAndDoctor method for AppointmentRepository

--- src/main/java/ru/shmelev/stomatologyapp/repository/AppointmentRepository.java
+++ src/main/java/ru/shmelev/stomatologyapp/repository/AppointmentRepository.java
@@ -1,7 +1,17 @@
 package ru.shmelev.stomatologyapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ru.shmelev.stomatologyapp.domain.Appointment;
 
+import java.util.List;
+
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+    @Query("""
+        select a from Appointment a
+        join fetch a.client
+        join fetch a.doctor d
+    """)
+    List<Appointment> findAllWithClientAndDoctor();
 }
