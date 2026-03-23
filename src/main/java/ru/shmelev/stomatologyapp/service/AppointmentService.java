d6c3a24252e417a72abc8e6e3689f65dd4700228
Savva <savahits@gmail.com>
2026-03-24 00:28:24 +0300

    feat: add findAll method for AppointmentService

--- src/main/java/ru/shmelev/stomatologyapp/service/AppointmentService.java
+++ src/main/java/ru/shmelev/stomatologyapp/service/AppointmentService.java
@@ -7,11 +7,14 @@ import ru.shmelev.stomatologyapp.domain.Appointment;
 import ru.shmelev.stomatologyapp.domain.Client;
 import ru.shmelev.stomatologyapp.domain.Doctor;
 import ru.shmelev.stomatologyapp.domain.User;
+import ru.shmelev.stomatologyapp.dto.AppointmentListItem;
 import ru.shmelev.stomatologyapp.dto.RequestAppointmentCreate;
 import ru.shmelev.stomatologyapp.dto.RequestClientCreate;
 import ru.shmelev.stomatologyapp.enums.AppointmentStatus;
 import ru.shmelev.stomatologyapp.repository.AppointmentRepository;
 import ru.shmelev.stomatologyapp.repository.DoctorRepository;
+
+import java.util.List;
 
 @Service
 public class AppointmentService {
@@ -29,7 +32,7 @@ public class AppointmentService {
 
     @Transactional
     public void create(RequestAppointmentCreate request, User currentUser) {
-        
+
         RequestClientCreate clientDto = new RequestClientCreate();
         clientDto.setName(request.getName());
         clientDto.setSurname(request.getSurname());
@@ -50,4 +53,19 @@ public class AppointmentService {
 
         appointmentRepository.save(appointment);
     }
+
+    public List<AppointmentListItem> findAll() {
+        return appointmentRepository.findAllWithClientAndDoctor()
+                .stream()
+                .map(a -> new AppointmentListItem(
+                        a.getId(),
+                        a.getClient().getSurname() + " " + a.getClient().getName(),
+                        a.getClient().getPhone(),
+                        a.getDoctor().getSurname() + " " + a.getDoctor().getName(),
+                        a.getAppointmentTime(),
+                        a.getStatus().name()
+                ))
+                .toList();
+    }
+
 }
