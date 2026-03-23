e96565d111b7fc11f84ade3075288a3cf34bbddf
Savva <savahits@gmail.com>
2026-03-24 00:02:05 +0300

    feat: create AppointmentController with: get createPage, post create

---
+++ src/main/java/ru/shmelev/stomatologyapp/controller/AppointmentController.java
@@ -0,0 +1,62 @@
+package ru.shmelev.stomatologyapp.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.shmelev.stomatologyapp.domain.User;
+import ru.shmelev.stomatologyapp.dto.RequestAppointmentCreate;
+import ru.shmelev.stomatologyapp.security.CustomUserDetails;
+import ru.shmelev.stomatologyapp.service.AppointmentService;
+import ru.shmelev.stomatologyapp.service.DoctorService;
+
+@Controller
+@RequestMapping("/appointments")
+@RequiredArgsConstructor
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+    private final DoctorService doctorService;
+
+    @GetMapping("/new")
+    public String createPage(Model model) {
+        model.addAttribute("appointment", new RequestAppointmentCreate());
+        model.addAttribute("doctors", doctorService.findAllDoctors());
+        return "appointments/new";
+    }
+
+    @PostMapping
+    public String create(
+            @ModelAttribute("appointment") @Valid RequestAppointmentCreate request,
+            BindingResult bindingResult,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            Model model
+    ) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("doctors", doctorService.findAllDoctors());
+            return "appointments/new";
+        }
+
+        if (userDetails == null) {
+            throw new RuntimeException("Unauthorized");
+        }
+
+        try {
+            User currentUser = userDetails.getUser();
+            appointmentService.create(request, currentUser);
+        } catch (RuntimeException e) {
+            bindingResult.reject("error.global", e.getMessage());
+            model.addAttribute("doctors", doctorService.findAllDoctors());
+            return "appointments/new";
+        }
+
+        return "redirect:/appointments";
+    }
+}
