5e73e2a584336e0671085edfa1e458a96318cf3c
Savva <savahits@gmail.com>
2026-03-29 19:39:26 +0300

    feat: add doctors/{id} get mapping

--- src/main/java/ru/shmelev/stomatologyapp/controller/DoctorController.java
+++ src/main/java/ru/shmelev/stomatologyapp/controller/DoctorController.java
@@ -6,10 +6,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.shmelev.stomatologyapp.dto.RequestDoctorCreate;
 import ru.shmelev.stomatologyapp.exception.UsernameAlreadyExistsException;
 import ru.shmelev.stomatologyapp.repository.SpecializationRepository;
@@ -34,6 +31,13 @@ public class DoctorController {
         model.addAttribute("doctors", doctorService.findAllDoctors());
 
         return "doctors/index";
+    }
+
+    @GetMapping("/{id}")
+    public String getDoctorById(@PathVariable Long id, Model model) {
+        model.addAttribute("doctor", doctorService.findById(id));
+
+        return "doctors/show";
     }
 
     @GetMapping("/new")
