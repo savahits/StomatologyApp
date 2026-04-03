04f24963233e7615c1d1645dfd70e9701c007777
Savva <savahits@gmail.com>
2026-04-03 12:32:20 +0300

    refactor: change AppointmentListItem from class to record

--- src/main/java/ru/shmelev/stomatologyapp/dto/AppointmentListItem.java
+++ src/main/java/ru/shmelev/stomatologyapp/dto/AppointmentListItem.java
@@ -1,25 +1,15 @@
 package ru.shmelev.stomatologyapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-public class AppointmentListItem {
-
-    private Long id;
-
-    private String clientFullName;
-    private String clientPhone;
-
-    private Long doctorId;
-    private String doctorFullName;
-
-    private LocalDateTime time;
-
-    private boolean beenBefore;
-
-    private String status;
+public record AppointmentListItem(
+    Long id,
+    String clientFullName,
+    String clientPhone,
+    Long doctorId,
+    String doctorFullName,
+    LocalDateTime time,
+    boolean beenBefore,
+    String status
+) {
 }
