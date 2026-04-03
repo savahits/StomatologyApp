93f30a83191bbc823bb22019d755a983941f4b5e
Savva <savahits@gmail.com>
2026-04-03 21:29:37 +0300

    feat: add @Param("status") AppointmentStatus status for findAllWithClientAndDoctor method

--- src/main/java/ru/shmelev/stomatologyapp/repository/AppointmentRepository.java
+++ src/main/java/ru/shmelev/stomatologyapp/repository/AppointmentRepository.java
@@ -2,19 +2,21 @@ package ru.shmelev.stomatologyapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.shmelev.stomatologyapp.domain.Appointment;
+import ru.shmelev.stomatologyapp.enums.AppointmentStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
-    @Query("""
-        select a from Appointment a
-        join fetch a.client
-        join fetch a.doctor d
-    """)
-    List<Appointment> findAllWithClientAndDoctor();
+    @Query("select a from Appointment a " +
+            "join fetch a.client " +
+            "join fetch a.doctor d " +
+            "where a.status = :status")
+    List<Appointment> findAllWithClientAndDoctor(@Param("status") AppointmentStatus status);
+
 
     @Query("""
     select a from Appointment a
