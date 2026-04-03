11d076130bf46a79b5a35e736c8cfcc05dbaddf2
Savva <savahits@gmail.com>
2026-04-03 14:10:31 +0300

    refactor: change RequestClientCreate from class to record class

--- src/main/java/ru/shmelev/stomatologyapp/dto/RequestClientCreate.java
+++ src/main/java/ru/shmelev/stomatologyapp/dto/RequestClientCreate.java
@@ -2,27 +2,21 @@ package ru.shmelev.stomatologyapp.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.Setter;
 import ru.shmelev.stomatologyapp.validator.ValidPhone;
 
-@Getter
-@Setter
-public class RequestClientCreate {
+public record RequestClientCreate(
+        @NotBlank
+        @Size(max = 100)
+        String surname,
 
-    @NotBlank
-    @Size(max = 100)
-    private String surname;
+        @NotBlank
+        @Size(max = 100)
+        String name,
 
-    @NotBlank
-    @Size(max = 100)
-    private String name;
+        @Size(max = 100)
+        String patronymic,
 
-    @Size(max = 100)
-    private String patronymic;
-
-    @NotBlank
-    @ValidPhone
-    private String phone;
-
-}
+        @NotBlank
+        @ValidPhone
+        String phone
+) {}
