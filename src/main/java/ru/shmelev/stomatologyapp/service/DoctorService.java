125697e2da9b4832a77b15f5535a4bfc23fbafb5
Savva <savahits@gmail.com>
2026-04-06 21:55:03 +0300

    feat: change findAllDoctors method: responses Page<Doctor>

--- src/main/java/ru/shmelev/stomatologyapp/service/DoctorService.java
+++ src/main/java/ru/shmelev/stomatologyapp/service/DoctorService.java
@@ -1,5 +1,7 @@
 package ru.shmelev.stomatologyapp.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import ru.shmelev.stomatologyapp.domain.Doctor;
 import ru.shmelev.stomatologyapp.dto.DoctorShowDTO;
 import ru.shmelev.stomatologyapp.dto.RequestDoctorCreate;
@@ -9,6 +11,8 @@ import java.util.List;
 public interface DoctorService {
 
     List<Doctor> findAllDoctors();
+
+    Page<Doctor> findAllDoctors(Pageable pageable);
 
     void create(RequestDoctorCreate dto);
 
