96a06ec728caa4284679f10e70a5dde40fa85400
Savva <savahits@gmail.com>
2026-04-09 16:29:30 +0300

    refactor: change super() message

--- src/main/java/ru/shmelev/stomatologyapp/exception/DoctorHasAppointmentsException.java
+++ src/main/java/ru/shmelev/stomatologyapp/exception/DoctorHasAppointmentsException.java
@@ -2,6 +2,6 @@ package ru.shmelev.stomatologyapp.exception;
 
 public class DoctorHasAppointmentsException extends RuntimeException {
     public DoctorHasAppointmentsException(Long id) {
-        super("Doctor with id=" + id + " has appointments");
+        super("У этого доктора есть запись");
     }
 }
