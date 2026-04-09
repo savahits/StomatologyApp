7753dd3f10102429cbd2c1df6292b961e651c001
Savva <savahits@gmail.com>
2026-04-09 17:01:26 +0300

    Revert "feat: add exception handler for EntityNotFoundException.class"
    This reverts commit 7687e91b08cb9ebf0ae4249097e9548299c8e99a.

--- src/main/java/ru/shmelev/stomatologyapp/exception/GlobalExceptionHandler.java
+++ src/main/java/ru/shmelev/stomatologyapp/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package ru.shmelev.stomatologyapp.exception;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -14,10 +13,5 @@ public class GlobalExceptionHandler {
 
         redirectAttributes.addFlashAttribute("error", ex.getMessage());
         return "redirect:/doctors";
-    }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public String handleNotFound(EntityNotFoundException ex) {
-        return "error/404";
     }
 }
