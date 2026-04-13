53a3d42bc8814f5390e0b2ab2ba468cd3ae62f4f
Savelii fedora <savahits@gmail.com>
2026-04-13 16:17:00 +0300

    feat: add existsByRoleName method for UserRepository

--- src/main/java/ru/shmelev/stomatologyapp/repository/UserRepository.java
+++ src/main/java/ru/shmelev/stomatologyapp/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package ru.shmelev.stomatologyapp.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.shmelev.stomatologyapp.domain.User;
 
 import java.util.Optional;
@@ -9,4 +11,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String name);
 
     boolean existsByUsername(String username);
+
+    @Query("SELECT COUNT(r) > 0 FROM Role r WHERE r.name = :roleName")
+    boolean existsByRoleName(@Param("roleName") String roleName);
 }
