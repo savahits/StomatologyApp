98266f2050fd4130a04fb3d5647ffb37e48bccaa
Savva <savahits@gmail.com>
2026-04-13 23:47:09 +0300

    refactor: move bean BCryptPasswordEncoder to own class PasswordConfig

--- src/main/java/ru/shmelev/stomatologyapp/security/SecurityConfig.java
+++ src/main/java/ru/shmelev/stomatologyapp/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -52,10 +51,5 @@ public class SecurityConfig {
                 );
 
         return http.build();
-    }
-
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
