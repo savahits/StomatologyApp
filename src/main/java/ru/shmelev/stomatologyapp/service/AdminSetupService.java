d6ca8698e2b0ba5e450e2e4c05b25d367151ec8e
Savelii fedora <savahits@gmail.com>
2026-04-13 16:17:57 +0300

    feat: add isAdminExists method for AdminSetupService

---
+++ src/main/java/ru/shmelev/stomatologyapp/service/AdminSetupService.java
@@ -0,0 +1,21 @@
+package ru.shmelev.stomatologyapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.shmelev.stomatologyapp.repository.UserRepository;
+
+@Service
+public class AdminSetupService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public AdminSetupService (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public boolean isAdminExists(){
+        return userRepository.existsByRoleName("ROLE_ADMIN");
+    }
+
+}
