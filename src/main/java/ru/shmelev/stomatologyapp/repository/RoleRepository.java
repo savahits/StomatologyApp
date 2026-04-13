8788d41660eaf164b8096ff5fd16c511133a94e4
Savva <savahits@gmail.com>
2026-04-13 23:47:39 +0300

    refactor: refactor findByName method

--- src/main/java/ru/shmelev/stomatologyapp/repository/RoleRepository.java
+++ src/main/java/ru/shmelev/stomatologyapp/repository/RoleRepository.java
@@ -3,8 +3,6 @@ package ru.shmelev.stomatologyapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.shmelev.stomatologyapp.domain.Role;
 
-import java.util.Optional;
-
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String roleDoctor);
+    Role findByName(String roleDoctor);
 }
