85b55ee3b6bd73905647e1b10818b5722c81e0a5
Savva <savahits@gmail.com>
2026-04-27 13:58:59 +0300

    fix: delete ClientDataMismatchException exception

--- src/main/java/ru/shmelev/stomatologyapp/exception/ClientDataMismatchException.java
+++
@@ -1,7 +0,0 @@
-package ru.shmelev.stomatologyapp.exception;
-
-public class ClientDataMismatchException extends RuntimeException {
-    public ClientDataMismatchException(String message) {
-        super(message);
-    }
-}
