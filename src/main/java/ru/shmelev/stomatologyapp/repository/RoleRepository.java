543e16507939dc3ce407b797bed15e7bc175a4d1
Savva <savahits@gmail.com>
2026-05-04 00:54:06 +0300

    refactor: change return type of findByName in RoleRepository to Optional for better null safety

--- src/main/java/ru/shmelev/stomatologyapp/repository/RoleRepository.java
+++ src/main/java/ru/shmelev/stomatologyapp/repository/RoleRepository.java
@@ -3,7 +3,9 @@ package ru.shmelev.stomatologyapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.shmelev.stomatologyapp.domain.Role;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String roleDoctor);
+    Optional<Role> findByName(String roleDoctor);
 
 }
